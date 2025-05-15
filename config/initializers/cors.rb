ee73ec44aa0c1f27f9b0ad6af439658f4c4efffc
pazs49 <percival49tv@gmail.com>
2025-05-16 01:10:18 +0800

    add proper origins url cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://stock-app-client-i4di.onrender.com"
 
     resource "*",
       headers: :any,
