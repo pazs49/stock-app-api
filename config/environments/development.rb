624d10b1a0c2670cb5d142c92a557c5212e31f4e
pazs49 <percival49tv@gmail.com>
2025-05-16 00:38:19 +0800

    remove caching mechanism

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,14 +17,14 @@ Rails.application.configure do
 
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
-  else
-    config.action_controller.perform_caching = false
-  end
+  # if Rails.root.join("tmp/caching-dev.txt").exist?
+  #   config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
+  # else
+  #   config.action_controller.perform_caching = false
+  # end
 
   # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  config.cache_store = :null_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
